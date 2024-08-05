bf2973b31c860a4eb033221858a55b09d15155ae
unknown <abishekpriyan11@gmail.com>
2024-08-04 21:24:38 -0400

    Modified app.js and index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EmployeeDirectory from './components/EmployeeDirectory';
+import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeDetails from './components/EmployeeDetails';
+import Navbar from './components/Navbar';
+import EmployeeList from './components/EmployeeList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<EmployeeDirectory />} />
+        <Route path="/employees/:type" element={<EmployeeList />} />
+        <Route path="/create-employee" element={<EmployeeCreate />} />
+        <Route path="/employee/:id" element={<EmployeeDetails />} />
+      </Routes>
+    </Router>
+  </ApolloProvider>
+);
 
 export default App;
